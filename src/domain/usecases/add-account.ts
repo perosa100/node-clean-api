bd490bef9341a7948c9949b4297ef2fb0a0e9ca5
Patrick Perosa <patrickperosapp@gmail.com>
2020-06-18 21:48:57 -0400

    feat: ensure SignUpController call AddAcounts with correct values

---
+++ src/domain/usecases/add-account.ts
@@ -0,0 +1,10 @@
+import { AccountModel } from '../models/account'
+export interface addAccountModel{
+  name: string
+  email: string
+  password: string
+}
+
+export interface AddAccount{
+  add (account: addAccountModel): AccountModel
+}
