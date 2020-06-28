f0aa04a3676fb883f83c7cd259163467a87dde9d
perosa100 <patrickperosapp@gmail.com>
2020-06-28 19:06:21 -0300

    test: ensure ValidateCOmposite returns the fisrt error if more then one validations fails

--- src/presentation/helpers/validators/validation-compose.spec.ts
+++ src/presentation/helpers/validators/validation-compose.spec.ts
@@ -11,22 +11,24 @@ const makeValidation = (): Validation => {
 }
 interface SutTypes {
   sut: ValidationCompose
-  validationStub: Validation
+  validationStubs: Validation[]
 }
 const makeSut = (): SutTypes => {
-  const validationStub = makeValidation()
-  const sut = new ValidationCompose([validationStub])
+  const validationStubs = [makeValidation(), makeValidation()]
+  const sut = new ValidationCompose(validationStubs)
   return {
     sut,
-    validationStub
+    validationStubs
   }
 }
 
 describe('Validation Composite', () => {
-  test('Should return an error if any validation fails', () => {
-    const { sut, validationStub } = makeSut()
-    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new MissingParamError('field'))
+  test('Should return the first error if more then one validation fails', () => {
+    const { sut, validationStubs } = makeSut()
+    jest.spyOn(validationStubs[0], 'validate').mockReturnValueOnce(new Error())
+    jest.spyOn(validationStubs[1], 'validate').mockReturnValueOnce(new MissingParamError('field'))
+
     const error = sut.validate({ field: 'any_value' })
-    expect(error).toEqual(new MissingParamError('field'))
+    expect(error).toEqual(new Error())
   })
 })
