8e910c993bba1fc4a4c4dbfc945cf097b701b8f2
perosa100 <patrickperosapp@gmail.com>
2020-07-12 14:41:36 -0300

    test: ensure AuthMiddleware calls LoadAccountByToken with correct token

--- src/presentation/middlewares/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -1,11 +1,41 @@
 import { AccessDeniedError } from './../errors'
 import { forbidden } from './../helpers/http/http-helper'
 import { AuthMiddleware } from './auth-middleware'
-
+import { LoadAccountByToken } from '../../domain/usecases/load-account-by-token'
+import { AccountModel } from '../../domain/models/account'
+const makeFakeAccount = (): AccountModel => ({
+  id: 'any_id',
+  name: 'any_name',
+  email: 'any_mail@mail.com',
+  password: 'hashed_password'
+})
 describe('Auth Middleware', () => {
   test('Should return 403 if no x-access-token exists in headers ', async () => {
-    const sut = new AuthMiddleware()
+    class LoadAccountByTokenStub implements LoadAccountByToken {
+      async load (accessToken: string, role?: string): Promise<AccountModel> {
+        return new Promise(resolve => resolve(makeFakeAccount()))
+      }
+    }
+    const loadAccountByTokenStub = new LoadAccountByTokenStub()
+    const sut = new AuthMiddleware(loadAccountByTokenStub)
     const httpResponse = await sut.handle({})
     expect(httpResponse).toEqual(forbidden(new AccessDeniedError()))
+  })
+
+  test('Should call LoadAccountByTojen if correct acessToken', async () => {
+    class LoadAccountByTokenStub implements LoadAccountByToken {
+      async load (accessToken: string, role?: string): Promise<AccountModel> {
+        return new Promise(resolve => resolve(makeFakeAccount()))
+      }
+    }
+    const loadAccountByTokenStub = new LoadAccountByTokenStub()
+    const loadSpy = jest.spyOn(loadAccountByTokenStub, 'load')
+    const sut = new AuthMiddleware(loadAccountByTokenStub)
+    await sut.handle({
+      headers: {
+        'x-access-token': 'any_token'
+      }
+    })
+    expect(loadSpy).toHaveBeenCalledWith('any_token')
   })
 })
