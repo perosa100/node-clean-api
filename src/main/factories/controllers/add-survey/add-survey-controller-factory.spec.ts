d09442c2c9d73ce8e773a03df50b33d5d31c0608
perosa100 <patrickperosapp@gmail.com>
2020-07-12 06:04:07 -0300

    feat: add AddSurveyCOmposition

---
+++ src/main/factories/controllers/add-survey/add-survey-controller-factory.spec.ts
@@ -0,0 +1,16 @@
+import { makeAddSurveyValidation } from './add-survey-validation-factory'
+import { ValidationCompose, RequiredFieldValidation } from '../../../../validation/validators'
+import { Validation } from '../../../../presentation/protocols/validation'
+
+jest.mock('../../../../validation/validators/validation-compose')
+
+describe('addSurveyValidation Factory', () => {
+  test('Should call ValidationCompose with all validations', () => {
+    makeAddSurveyValidation()
+    const validations: Validation[] = []
+    for (const field of ['question', 'answers']) {
+      validations.push(new RequiredFieldValidation(field))
+    }
+    expect(ValidationCompose).toHaveBeenCalledWith(validations)
+  })
+})
