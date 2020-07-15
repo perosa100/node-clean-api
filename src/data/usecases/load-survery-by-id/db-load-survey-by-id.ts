a2fe6395a4355ade508d6e8c1a92339c83e9efca
perosa100 <patrickperosapp@gmail.com>
2020-07-15 15:12:07 -0300

    feat: ensure DbLoadSurveyByID return a Survey if DbLoadSurveyByIdRepository success

--- src/data/usecases/load-survery-by-id/db-load-survey-by-id.ts
+++ src/data/usecases/load-survery-by-id/db-load-survey-by-id.ts
@@ -6,7 +6,7 @@ export class DbLoadSurveyById implements LoadSurveyById {
   constructor (private readonly loadSurveyByIdRepository: LoadSurveyByIdRepository) {}
 
   async loadById (id: string): Promise<SurveyModel> {
-    await this.loadSurveyByIdRepository.loadByID(id)
-    return null
+    const survey = await this.loadSurveyByIdRepository.loadByID(id)
+    return survey
   }
 }
