0f8a4fef072520125fb653b72eb0c24e7be884dc
perosa100 <patrickperosapp@gmail.com>
2020-07-16 02:34:41 -0300

    refactor: organize project files

---
+++ src/data/usecases/survey/load-survery-by-id/db-load-survey-by-id-protocols.ts
@@ -0,0 +1,3 @@
+export * from '@/domain/usecases/load-survey-by-id'
+export * from '@/presentation/controllers/survey/load-surveys/load-surveys-controller-protocols'
+export * from '@/data/protocols/db/survey/load-survey-by-id-repository'
