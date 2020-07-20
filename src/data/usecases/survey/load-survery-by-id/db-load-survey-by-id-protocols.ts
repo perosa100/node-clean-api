1508dc5d60d92d576b0e1288bd6c36a41b587f6e
perosa100 <patrickperosapp@gmail.com>
2020-07-20 18:23:52 -0300

    feat: ensure return 200 on save survey result with accessToken

--- src/data/usecases/survey/load-survery-by-id/db-load-survey-by-id-protocols.ts
+++ src/data/usecases/survey/load-survery-by-id/db-load-survey-by-id-protocols.ts
@@ -1,3 +1,3 @@
-export * from '@/domain/usecases/load-survey-by-id'
 export * from '@/presentation/controllers/survey/load-surveys/load-surveys-controller-protocols'
 export * from '@/data/protocols/db/survey/load-survey-by-id-repository'
+export * from '@/presentation/controllers/survey-result/survey-result/save-survey-result/save-survey-result-controller-protocols'
