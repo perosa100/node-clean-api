70d5ca5c0325cd4d93807c3fd729be39f6ab35b5
perosa100 <patrickperosapp@gmail.com>
2020-07-21 16:59:06 -0300

    docs: update api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,26 +1,27 @@
-import { loginParamsShema } from './schemas/login-params-schema'
-import { accountSchema } from './schemas/account-schema'
-import { loginPath } from './paths/login-path'
+import paths from './paths'
+import components from './components'
+import schemas from './schemas'
 
 export default {
   openapi: '3.0.0',
   info: {
-    title: 'Clean Node Api',
-    description: 'Aprendizado com TDD para realizar enquetes entre programadores',
+    title: 'Clean Node API',
+    description: 'API do curso do Mango para realizar enquetes entre programadores',
     version: '1.0.0'
+  },
+  license: {
+    name: 'GPL-3.0-or-later',
+    url: 'https://spdx.org/licenses/GPL-3.0-or-later.html'
   },
   servers: [{
     url: '/api'
   }],
   tags: [{
     name: 'Login'
+  }, {
+    name: 'Enquete'
   }],
-  paths: {
-    '/login': loginPath
-  },
-  schemas: {
-    account: accountSchema,
-    loginParams: loginParamsShema
-  }
-
+  paths,
+  schemas,
+  components
 }
--- src/main/docs/schemas/login-params-schema.ts
+++ src/main/docs/schemas/login-params-schema.ts
@@ -1,4 +1,4 @@
-export const loginParamsShema = {
+export const loginParamsSchema = {
   type: 'object',
   properties: {
     email: {
